6e306f6fb31108bf819785a3ccb87c8f11938a30
CimarSantos <francimarlsantos@outlook.com>
2022-11-22 17:03:40 -0300

    Componente do Jogo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import Jogo from "./Jogo";
+
+import reset from "../styles/reset.css";
+import app from "../styles/app.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Jogo />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Jogo.js
@@ -0,0 +1,22 @@
+import Forca0 from "../assets/forca0.png";
+import Forca1 from "../assets/forca1.png";
+import Forca2 from "../assets/forca2.png";
+import Forca3 from "../assets/forca3.png";
+import Forca4 from "../assets/forca4.png";
+import Forca5 from "../assets/forca5.png";
+import Forca6 from "../assets/forca6.png";
+
+const Jogo = () => {
+  return (
+    <>
+      <div className="jogobox">
+        <img className="forca" src={Forca0} alt="Imagem da Forca" />
+
+        <button>Escolher Palavra</button>
+        <h3>______</h3>
+      </div>
+    </>
+  );
+};
+
+export default Jogo;
